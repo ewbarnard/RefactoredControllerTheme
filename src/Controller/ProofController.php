09fb5d8465014d5b2b42617c506ce18538103dcf
Edward Barnard <ewbarnard@embarqmail.com>
2015-04-26 15:12:02 -0500

    The generated plugin dependencies do not match versions with the main project

---
+++ src/Controller/ProofController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace RefactoredControllerTheme\Controller;
+
+//use App\Controller\AppController as BaseController;
+
+class ProofController extends AppController
+{
+	public function index() {
+		pr(array(__FILE__,__FUNCTION__,__LINE__));
+		exit;
+	}
+	public function proof() {
+		pr(array(__FILE__,__FUNCTION__,__LINE__));
+		exit;
+	}
+
+}
