1f43c04e51db0d8b96749429a0180fab76355a70
Edward Barnard <ewbarnard@embarqmail.com>
2015-04-26 16:12:32 -0500

    use list of allowed views

--- src/Controller/ProofController.php
+++ src/Controller/ProofController.php
@@ -2,10 +2,14 @@
 
 namespace RefactoredControllerTheme\Controller;
 
-//use App\Controller\AppController as BaseController;
+use App\Controller\AppController as BaseController;
 
 class ProofController extends AppController
 {
+	protected $allowedViews = array(
+		'index', 'proof',
+	);
+
 	public function index() {
 		pr(array(__FILE__,__FUNCTION__,__LINE__));
 		exit;
